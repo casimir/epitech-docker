21ade3a9f69c55c15364131396d3bde9a76f7dd9
Martin Chaine <chaine.martin@gmail.com>
2015-06-11 12:38:25 +0200

    Update build script

--- blinux-make/make_and_test.sh
+++ blinux-make/make_and_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-./norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> norm.dpr 2> norm.note &&\
-make -C rendu   && \
-cp -rn rendu/* . && \
+./norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> norm.dpr 2> norm.note
+make -C rendu
+cp -rn rendu/* .
 bugs -out "wud xunit"
