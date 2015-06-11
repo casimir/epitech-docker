552463e5a133189a32777d4c38db4630087d4aef
Martin Chaine <chaine.martin@gmail.com>
2015-06-11 14:53:24 +0200

    Fix norme?

--- blinux-make/make_and_test.sh
+++ blinux-make/make_and_test.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -xe
-./norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> norm.dpr 2> norm.note
+#!/bin/sh -xe
+
+~/norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> ~/workspace/norm.dpr 2> ~/workspace/norm.note
 make -C rendu
 cp -rn rendu/* .
 bugs -out "wud xunit"
