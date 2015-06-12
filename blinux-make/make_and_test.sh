0d0ba2c0d266c97812f74dadca01243308d72808
Martin Chaine <chaine.martin@gmail.com>
2015-06-12 09:04:38 +0200

    Add txt output to bugs

--- blinux-make/make_and_test.sh
+++ blinux-make/make_and_test.sh
@@ -3,4 +3,4 @@
 ~/norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> ~/workspace/norm.dpr 2> ~/workspace/norm.note
 make -C rendu
 cp -rn rendu/* .
-bugs -out "wud xunit"
+bugs -out "txt wud xunit"
