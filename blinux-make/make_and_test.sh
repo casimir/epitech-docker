80b7fd8c10be6a140026c63e672316fe003436d1
Martin Chaine <chaine.martin@gmail.com>
2015-06-15 17:54:07 +0200

    Optional makefile

--- blinux-make/make_and_test.sh
+++ blinux-make/make_and_test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -xe
 
 ~/norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> ~/workspace/norm.dpr 2> ~/workspace/norm.note
-make -C rendu
+if [ -f Makefile ]; then
+    make -C rendu
+fi
 cp -rn rendu/* .
 bugs -out "graph txt xunit" -login $DBUSER
