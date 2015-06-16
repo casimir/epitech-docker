604c914cda7f31e298bf8f752e1fface45838ed6
Axelle Ziegler <axelle.ziegler@epitech.eu>
2015-06-16 12:40:26 +0200

    ville dans la moulinette

--- blinux-make/make_and_test.sh
+++ blinux-make/make_and_test.sh
@@ -1,8 +1,8 @@
-#!/bin/sh -xe
+#!/bin/sh -x
 
 ~/norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> ~/workspace/norm.dpr 2> ~/workspace/norm.note
 if [ -f Makefile ]; then
     make -C rendu
 fi
 cp -rn rendu/* .
-bugs -out "graph txt xunit" -login $DBUSER
+bugs -out "graph txt xunit" -login $DBUSER -city $CITY
