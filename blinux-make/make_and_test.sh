26c4681b6808c81b114128a5aad0c18cbdf4145b
Martin Chaine <chaine.martin@gmail.com>
2015-06-30 20:10:57 +0200

    fixpath

--- blinux-make/make_and_test.sh
+++ blinux-make/make_and_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 ~/norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> ~/workspace/norm.dpr 2> ~/workspace/norm.note
-if [ -f Makefile ]; then
+if [ -f rendu/Makefile ]; then
     make -C rendu
 fi
 cp -rn rendu/* .
