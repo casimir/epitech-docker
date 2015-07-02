ff33cf59d05ff248f9ccb0819c6582625528b331
Martin Chaine <chaine.martin@gmail.com>
2015-07-02 16:59:45 +0200

    Bump bugs

--- blinux-make/make_and_test.sh
+++ blinux-make/make_and_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 ~/norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> ~/workspace/norm.dpr 2> ~/workspace/norm.note
-[ -f Makefile ] && cp rendu/prerequis.c . && make
+[ -f Makefile ] && make
 [ -f rendu/Makefile ] && make -C rendu
 cp -rn rendu/* .
 bugs -out "graph txt xunit" -login $DBUSER -city $CITY
